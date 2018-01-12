56e020e80dc59a91ec83924220bdd4eebf5a554f
Helena Brunsgaard Madsen <hele1564@student.eadania.dk>
2018-01-12 12:40:00 +0100

    ShowDetails class

--- KHMB/KHMB/Show.xaml.cs
+++ KHMB/KHMB/Show.xaml.cs
@@ -29,16 +29,11 @@ namespace KHMB
             //Get info from db of the chosen object from drop down.
             DB.CloseConnection();
         }
-        public static void ShowDetails()
-        {
-            //Show details of the chosen job/whatever when clicking on job.
-        }
+
         private void btn_Show_Click(object sender, RoutedEventArgs e)
         {
-            //Show information of the chosen object.
-            //(Klaus) Skal ændres til: Åbne nyt vindue med baseret på hvad der skal vises??
-            MainWindow returnWindow = new MainWindow();
-            returnWindow.Show();
+            ShowDetails sd = new ShowDetails();
+            sd.Show();
             this.Close();
         }
 
