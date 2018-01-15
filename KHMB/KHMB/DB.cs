642078dcdf8c58a0869659ac7c72d36f45ddc695
Mikkel Glerup <Mikkel Glerup@MIKKELGLERUP>
2018-01-15 14:16:50 +0100

    FIlled out InsertRT

--- KHMB/KHMB/DB.cs
+++ KHMB/KHMB/DB.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
 
 
 namespace KHMB
@@ -25,7 +28,9 @@ namespace KHMB
         public static void InsertRT(string RT)
         {
             OpenConnection();
-
+            SqlCommand cmd = new SqlCommand(
+            string.Format("INSERT INTO ResourceType (Name) VALUES (,{0}')", RT), myConnection);
+            cmd.ExecuteNonQuery();
         }
     }
 
